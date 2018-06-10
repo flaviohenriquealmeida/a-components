5ce97c5042fd10e83f6cb92bcd2bb6632090d5b9
Flávio Almeida <flaviohenriquealmeida@gmail.com>
2018-06-10 14:57:56 -0300

    update

--- src/app/shared/checkbox/checkbox.component.ts
+++ src/app/shared/checkbox/checkbox.component.ts
@@ -1,5 +1,4 @@
 // https://github.com/angular/angular/issues/17495
-
 import { Component, Output, EventEmitter, Input, forwardRef } from "@angular/core";
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS } from "@angular/forms";
 
@@ -7,44 +6,31 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR, NG_VALIDATORS } from "@angular
     selector: 'my-checkbox',
     templateUrl: './checkbox.component.html',
     providers: [
-        { 
-          provide: NG_VALUE_ACCESSOR,
-          useExisting: forwardRef(() => CheckboxComponent),
-          multi: true
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => CheckboxComponent),
+            multi: true
         }
-      ]    
+    ]
 })
 export class CheckboxComponent implements ControlValueAccessor {
 
-    @Input() checked:boolean = false;
+    @Input() checked: boolean = false;
     @Output() onChange = new EventEmitter<boolean>();
-    
-    propagateChange = (_: any) => {};
 
-    /* This method gets called when the form is initialized, 
-    with the form model’s initial value. This means it will override 
-    the default value 0, which is fine but if we think about the simple 
-    form setup we talked about earlier, we realise that there is 
-    no initial value in the form model:
-    */
+    propagateChange = (_: any) => { };
+
+
     writeValue(value: any): void {
-        console.log('writeValue', value);
-        this.checked = value;
+        this.checked = !!value;
     }
-    
-    /* 
-        We don’t want to do anything at this event, so we can implement 
-        the interface with an empty function.
-    */
+
     registerOnChange(fn: any): void {
-        console.log('registerOnChange');
         this.propagateChange = fn;
     }
-    registerOnTouched(fn: any): void {}
+    registerOnTouched(fn: any): void { }
 
-    setDisabledState?(isDisabled: boolean): void {
-        //this.disabled = isDisabled;
-    }
+    setDisabledState?(isDisabled: boolean): void { }
 
     toogle() {
         this.checked = !this.checked;
